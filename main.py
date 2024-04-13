8720e5166addaed38b3ee5b2d11e58541d33cce6
Marvin-Deng <52214624+Marvin-Deng@users.noreply.github.com>
2024-04-12 20:45:19 -0700

    feat: Add function to recursively parse cloned repo

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from file_utils import clone_repo
+from file_utils import (
+    clone_repo,
+    recursively_parse_repo_files
+)
 
 def main():
     github_url = "https://github.com/Marvin-Deng/Online-Store"
     repo_path = "./repo/Online-Store"
     clone_repo(github_url, repo_path)
+    recursively_parse_repo_files(repo_path)
 
 if __name__ == '__main__':
     main()
