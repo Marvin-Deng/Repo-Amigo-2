c085a8f0936cfdeedc3f999467ee61baa536ab3d
Marvin-Deng <52214624+Marvin-Deng@users.noreply.github.com>
2024-04-12 19:59:24 -0700

    feat: Add text chunk splitter function

--- file_utils.py
+++ file_utils.py
@@ -1,8 +1,8 @@
 from git import Repo
-import os
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
-def clone_repo(github_url: str, repo_path: str, token=None):
+def clone_repo(github_url: str, repo_path: str, token=None) -> bool:
     try:
         if token:
             formatted_url = f"https://{token}@{github_url.split('https://')[1]}"
@@ -15,3 +15,9 @@ def clone_repo(github_url: str, repo_path: str, token=None):
     except Exception as e:
         print(f"Failed to clone repository: {e}")
         return False
+
+
+def get_text_chunks(text: str) -> list:
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
